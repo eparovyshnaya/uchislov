e5f40f353ca2cf39430b2cb7b8915a3d31191a26
eparovyshnaia <elena.parovyshnaia@arsysop.ru>
2026-03-02 12:47:59 +0300

    - add new 'Latin' module with tests - add license headers to source files

--- js/SearchTestForm.js
+++ js/SearchTestForm.js
@@ -1,3 +1,22 @@
+/*
+* Copyright (c) AParovyshnaya 2020-2026
+* 
+* AParovyshnaya and theirs affiliates make no warranty of any kind
+* with regard to this material. 
+* 
+* AParovyshnaya expressly disclaims all warranties as to the material, express, 
+* and implied, including but not limited to the implied warranties of 
+* merchantability, fitness for a particular purpose and non-infringement of third 
+* party rights. 
+* 
+* In no event shall AParovyshnaya be liable to you or any other person for any damages, 
+* including, without limitation, any direct, indirect, incidental or consequential 
+* damages, expenses, lost profits, lost data or other damages arising out of the use, 
+* misuse or inability to use the material and any derived software, even if AParovyshnaya, 
+* their affiliate or an authorized dealer has been advised of the possibility of such damages. 
+* 
+*/
+
 function filter(data) {
   generateResults(takeData());
 }
