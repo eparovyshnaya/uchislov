7c32209ea88044743525f0037f813ba8241c9d68
eparovyshnaia <elena.parovyshnaia@arsysop.ru>
2026-08-02 12:21:14 +0300

    cum Juliae

---
+++ js/latin/tests_julia.js
@@ -0,0 +1,96 @@
+/*
+* Copyright (c) AParovyshnaya 2020-2026
+* 
+* AParovyshnaya and theirs affiliates make no warranty of any kind
+* with regard to this material. 
+* 
+* AParovyshnaya expressly disclaims all warranties as to the material, express, 
+* and implied, including but not limited to the implied warranties of 
+* merchantability, fitness for a particular purpose and non-infringement of third 
+* party rights. 
+* 
+* In no event shall AParovyshnaya be liable to you or any other person for any damages, 
+* including, without limitation, any direct, indirect, incidental or consequential 
+* damages, expenses, lost profits, lost data or other damages arising out of the use, 
+* misuse or inability to use the material and any derived software, even if AParovyshnaya, 
+* their affiliate or an authorized dealer has been advised of the possibility of such damages. 
+* 
+*/
+
+export function withJulia() {
+	data.data.push(
+		{
+			"name": "Julia I, 1",
+			"id": "julia-i-1",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "Iulia puella parva est. Prope oram maritimam habitat. Britannia est Juliae patria. Britannicae oram maritimam amant. Nautas quoque amant puellae Britannicae.",
+			"test": "Iulia puell<…> parv<…> est. Prope or<…> maritim<…> habit<…>. Britannia est Iuli<…> patria. Britannicae or<…> maritim<…> amant. Naut<…> quoque amant puellae Britannicae."
+		},
+		{
+			"name": "Julia I, 2",
+			"id": "julia-i-2",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "Iulia est filia agricolae et casam parvam habitat. Sed Iulia oram maritimam et nautas amat. Nautae quoque Iuliam amant. Saepe prope oram maritimam Iulia ambulat. Nautarum filiae cum Iulia ambulant et prope oram maritimam saltant.",
+			"test": "Iulia est filia agricol<…> et cas<…> parv<…> habit<…>. Sed Iulia or<…> maritim<…> et naut<…> ama<…>. Nautae quoque Iuli<…> ama<…>. Saepe prope or<…> maritim<…> Iulia ambul<…>. Naut<…> filiae cum Iuli<…> ambula<…> et prope or<…> maritim<…> salta<…>."
+		},
+		{
+			"name": "Julia I, 3",
+			"id": "julia-i-",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "Multae rosae sunt prope Iuliae casam. Rosis aquam dat Iulia. Saepe Iulia rosas nautis dat. Agricora Iuliam non culpat sed laudat quod rosas pulchras nautis dat.",
+			"test": "Mul<…> ros<…> sunt prope Iuli<…> cas<…>. Ros<…> aqu<…> da<…> Iulia. Saepe Iulia ros<…> naut<…> dat. Agricora Iuli<…> non culpa<…> sed lauda<…> quod ros<…> pulchr<…> naut<…> dat."
+		},
+		{
+			"name": "Julia I, 4",
+			"id": "julia-i-4",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "Rubrae et albae sunt rosae. Saepe Iulia ad nautarum casas rosas puchras portat. Nautae puellam parvam laudant.",
+			"test": "Rubr<…> et alb<…> sunt ros<…>. Saepe Iulia ad naut<…> cas<…> ros<…> puchr<…> porta<…>. Nautae puell<…> parv<…> lauda<…>."
+		},
+		{
+			"name": "Julia II, 1",
+			"id": "julia-ii-1",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "Ad Iuliae casam pirata venit. Rubra est piratae tunica, spliendidae sunt galea et hasta. Iulia prope casae partam stat et piratam spectat; piratae galeam et hastam et tunicam rubram amat et laudat.",
+			"test": "Ad Iuli<…> cas<…> pirata ven<…>. Rubr<…> est pirat<…> tunica, spliendid<…> sunt galea et hasta. Iulia prope cas<…> port<…> sta<…> et pirat<…> specta<…>; pirat<…> gale<…> et hast<…> et tunic<…> rubr<…> ama<…> et lauda<…>."
+		},
+		{
+			"name": "Julia II, 2",
+			"id": "julia-ii-2",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "Pirata quoque Iuliam et casam et rosas laudat. 'O Iulia', inquit, 'pulchra es puella et pulchrae sunt rosae tuae. Navicula mea pulchra est. Alta est prora naviculae meae. Et extremA naviculA sto et naviculam guberno. Alba est navicula mea, nunc prope oram maritimam stat.'",
+			"test": "Pirata quoque Iuli<…> et cas<…> et ros<…> lauda<…>. 'O Iulia', inqu<…>, 'pulchra es puella et pulchr<…> sunt ros<…> tu<…>. Navicula mea pulchr<…> est. Alt<…> est prora navicul<…> me<…>. Et extremA naviculA sto et navicul<…> gubern<…>. Alb<…> est navicula mea, nunc prope or<…> maritim<…> sta<…>.'"
+		},
+		{
+			"name": "Julia II, 3",
+			"id": "julia-ii-3",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "Tum Iulia cum pirata ad oram maritimam ambulat et naviculam albam spectat. Iulia et pirata proram naviculae multis rosis ornant. Subito pirata puellam in naviculam iactat. Multae sunt lacrimae puellae, sed frustra - pirata et extremA naviculA stat et naviculam gubernat.",
+			"test": "Tum Iulia cum pirata ad or<…> maritim<…> ambula<…> et navicul<…> alb<…> specta<…>. Iulia et pirat<…> pror<…> navicul<…> mult<…> ros<…> orna<…>. Subito pirata puell<…> in navicul<…> iacta<…>. Mult<…> sunt lacrim<…> puell<…>, sed frustra - pirata et extremA naviculA sta<…> et navicul<…> guberna<…>."
+		},
+		{
+			"name": "Julia III, 1",
+			"id": "julia-iii-1",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "",
+			"test": ""
+		},
+		{
+			"name": "Julia III, ",
+			"id": "julia-iii-",
+			"grade": "latin_simple",
+			"letters": ["1", "I"],
+			"full": "",
+			"test": ""
+		}
+	);
+
+}
--- js/latin/tests_latin_module.js
+++ js/latin/tests_latin_module.js
@@ -22,6 +22,7 @@ import { withLatinNoun2Tests } from './tests_latin_noun_2.js';
 import { withLatinNoun3Tests } from './tests_latin_noun_3.js';
 import { withLatinNoun4Tests } from './tests_latin_noun_4.js';
 import { withLatinNoun5Tests } from './tests_latin_noun_5.js';
+import { withJulia } from './tests_julia.js';
 
 // tests for latin ending of nouns: 5 declensions, singular and plural, 5 cases (nominative, genetive, dative, accusative, ablative)
 withLatinNoun1Tests();
@@ -29,3 +30,7 @@ withLatinNoun2Tests();
 withLatinNoun3Tests();
 withLatinNoun4Tests();
 withLatinNoun5Tests();
+
+// texts from "Julia, a Latin reading book reatten by Maud Reed"
+withJulia();
+
